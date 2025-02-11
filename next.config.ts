0e9a68a72169d88fefafa31fab5c1e5a1ebd371e
OSWarped <37671707+OSWarped@users.noreply.github.com>
2025-02-11 12:51:06 -0600

    fixed issues

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["momoslimes.com", "cdn.mall.adeptmind.ai"], // ✅ Add the domain here
+  },
 };
 
 export default nextConfig;
